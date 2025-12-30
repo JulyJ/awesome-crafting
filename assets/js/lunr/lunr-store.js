e09d8c12dbb55b6e59808d8bbc4610ffa3c725de
JulyJ <JulyJ@users.noreply.github.com>
2025-12-30 05:34:22 +0000

    deploy: 4a6e9ffe4e1810e47ce186c96808233ebf1bc6d3

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to Crafting Supply Hub",
+        "excerpt":"Welcome to Crafting Supply Hub! This blog will help Canadian crafters find the best supplies and tools.   Stay tuned for tips, calculators, and product recommendations!  ","categories": [],
+        "tags": [],
+        "url": "/2025/06/22/welcome-to-crafting-supply.html",
+        "teaser": null
+      },{
+        "title": "Spotlight: Canadian-Made Craft Supplies",
+        "excerpt":"Discover the best Canadian-made craft supplies and support local makers! For a full list of Canadian-made materials, visit the Canadian-Made Materials page.  ","categories": [],
+        "tags": ["canadian-made"],
+        "url": "/2025/06/24/spotlight-canadian-made.html",
+        "teaser": null
+      }]
